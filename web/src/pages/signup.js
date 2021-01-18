0231512a0158f66c1f5d366acf36440272644507
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-01-18 23:32:00 +0800

    signup mutation sucess

--- web/src/pages/signup.js
+++ web/src/pages/signup.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
-import Button from "../components/Button";
-import styled from "styled-components";
+import React, { useEffect, useState } from 'react';
+import Button from '../components/Button';
+import styled from 'styled-components';
+import { useMutation, useApolloClient, gql } from '@apollo/client';
 
 const Wrapper = styled.div`
   border: 1px solid #f5f4f0;
@@ -22,9 +23,16 @@ const Form = styled.form`
   }
 `;
 
+const SIGNUP_USER = gql`
+  mutation signUp($email: String!, $username: String!, $password: String!) {
+    signUp(email: $email, username: $username, password: $password)
+  }
+`;
+
 // include the props passed to the component for later use
 const SignUp = props => {
   // set the default state of the form
+  //   useState, onChange, and useEffect all remain the same here
   const [values, setValues] = useState();
 
   // update the state when a user types in the form
@@ -36,21 +44,39 @@ const SignUp = props => {
     });
   };
 
-
   useEffect(() => {
     // update the document title
-    document.title = "Sign Up — Notedly";
+    document.title = 'Sign Up — Notedly';
   });
 
+  /** doc link: https://www.apollographql.com/docs/react/data/mutations/
+   *   When your component renders, useMutation returns a tuple that includes:
+   *   - A mutate function
+   *   - An object with fields that represent the current status of the mutation's execution
+   */
+  const [signUp, { loading, error }] = useMutation(SIGNUP_USER, {
+    onCompleted: data => {
+      //  add mutation hook
+      console.log(data, data.signUp);
+    }
+  });
+
+  // render our form
   return (
     <Wrapper>
       <h2>Sign Up</h2>
+      {/* pass the form data to the mutation when a user submits the form */}
       <Form
         onSubmit={event => {
           event.preventDefault();
-          console.log(values);
+          signUp({
+            variables: {
+              ...values
+            }
+          });
         }}
       >
+        {/* ... the rest of the form remains unchanged ... */}
         <label htmlFor="username">Username:</label>
         <input
           required
